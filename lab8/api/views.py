31e7ba18fc01739f503172f5f5cdbc921f4a27f5
yernar2772 <dante.foster2017@gmail.com>
2022-04-03 19:12:31 +0600

    Fix(views): Fix with get_object_by_id function

--- lab8/api/views.py
+++ lab8/api/views.py
@@ -4,17 +4,21 @@ from django.http import JsonResponse
 from .models import Product, Category
 
 
+def get_object_by_id(model, object_id: int):
+    try:
+        object_from_db = model.objects.get(id=object_id)
+    except model.DoesNotExist as e:
+        return JsonResponse({'data': str(e)})
+    return JsonResponse(model_to_dict(object_from_db))
+
+
 def get_product_list(request):
     product_list = Product.objects.all()
     return JsonResponse({'data': [model_to_dict(product) for product in product_list]})
 
 
 def get_product_by_id(request, product_id: int):
-    try:
-        product = Product.objects.get(id=product_id)
-    except Product.DoesNotExist as e:
-        return JsonResponse({'data': str(e)})
-    return JsonResponse(model_to_dict(product))
+    return get_object_by_id(model=Product, object_id=product_id)
 
 
 def get_category_list(request):
@@ -23,8 +27,4 @@ def get_category_list(request):
 
 
 def get_category_by_id(request, category_id: int):
-    try:
-        category = Category.objects.get(id=category_id)
-    except Category.DoesNotExist as e:
-        return JsonResponse({'data': str(e)})
-    return JsonResponse(model_to_dict(category))
+    return get_object_by_id(model=Category, object_id=category_id)
